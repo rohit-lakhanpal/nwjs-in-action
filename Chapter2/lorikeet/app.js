88f0f7cd278246a46339d39e1e42340b53cfa6b9
rohitl <rohitl@panvivacom>
2015-12-09 20:54:00 +1100

    Using the osenv node package, added functionality to return the user's home folder path.

--- Chapter2/lorikeet/app.js
+++ Chapter2/lorikeet/app.js
@@ -1,10 +1,21 @@
- // Load Node.js module "os"
- var os = require("os");
- 
- // This function detects the OS Platform version
- var detectOs = function (){
-	 return os.platform();
- }
- 
- // Alert OS
- alert(detectOs());
+// Load Node.js module "os"
+var os = require("os");
+
+// Load the osenv package
+var osenv = require("osenv");
+
+// This function detects the OS Platform version
+var detectOs = function (){
+	return os.platform();
+}
+
+// This function returns the user's home folder path 
+var getUsersHomeFolder = function () {
+	return osenv.home();
+} 
+
+// Alert OS
+alert(detectOs());
+
+// Alert home folder
+alert(getUsersHomeFolder());
